f77d6dde8c7ae49152d1d873273e06ef0fc38817
Joshua Castasneda <josh@Joshuas-MacBook-Pro-5.local>
2025-02-15 15:57:52 -0800

    learning some python basics and studying AI code base

---
+++ CPSC 481/me.py
@@ -0,0 +1,32 @@
+firstName = "Joshua"
+lastName = "Castaneda"
+fullName = f"{firstName} {lastName}"
+
+print(fullName)
+
+newName = input("What is your name: ").lower()
+
+print(f"Whats up {newName.title()}? damn you cute as hell")
+
+favoriteThings = ["stuff", "tings", "and stuff", 1, 2, 3, 4, 5]
+
+newFavoriteThing = input("Whats your favorite thing? ")
+
+ 
+favoriteThings.append(newFavoriteThing)
+
+print(favoriteThings)
+
+firstThing = favoriteThings.pop(0)
+
+
+
+print(favoriteThings)
+
+print(firstThing)
+
+for thing in favoriteThings:
+    print(thing)
+
+number = list(range(10))
+print(number)
