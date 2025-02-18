30c1f1f8d7ff97220dad5cee871d4d396944feeb
Joshua Castasneda <josh@Joshuas-MacBook-Pro-5.local>
2025-02-18 14:38:38 -0800

    Finished AI project 1

--- CPSC 481/project1.py
+++ CPSC 481/project1.py
@@ -22,51 +22,73 @@ class MissCannibalsVariant(Problem):
             return state == self.goal
     
     def actions(self, state):
+        
         possible_actions = ['M', 'C', 'MM', 'MC', 'CC', 'MMM', 'MMC', 'CCC']
+        
         boatIsAtLeftBank = state[2]
+        missionariesOnLeftBank = state[0]
+        cannibalsOnLeftBank = state[1]
 
+        missionariesOnRightBank = self.N1 - state[0]
+        cannibalsOnRightBank = self.N2 - state[1]
+
+        """ 1st: Remove actions that have a greater number of Missionaries or Cannibals than actually available"""
         if boatIsAtLeftBank:
             for action in possible_actions[:]:
-                m = state[0] - action.count('M')
-                c = state[1] - action.count('C')
-                if m < c:
+                if action.count('M') > missionariesOnLeftBank and action in possible_actions:
+                    possible_actions.remove(action)
+                if action.count('C') > cannibalsOnLeftBank and action in possible_actions:
                     possible_actions.remove(action)
         else:
-
-            nmrs = self.N1 - state[0]
-            ncrs = self.N2 - state[1]
-
             for action in possible_actions[:]:
-                if action.count('M') > nmrs:
+                if action.count('M') > missionariesOnRightBank and action in possible_actions:
                     possible_actions.remove(action)
-                if action.count('C') > ncrs:
+                if action.count('C') > cannibalsOnRightBank and action in possible_actions:
                     possible_actions.remove(action)
-
+        
+        """2nd: Remove actions that would make MissonariesOnRightBank < CannibalsOnRightBank or MissionariesOnLeftBank < CannibalsOnLeftBank"""
+        if boatIsAtLeftBank:
             for action in possible_actions[:]:
-                m = state[0] + action.count('M')
-                c = state[1] + action.count('C')
-                if m < c:
+                """Checking if the number of cannibals to be sent to the right bank will out number the missionaries on the right bank but its okay if missionaries == 0 at right bank after action"""
+                if action.count('C') + cannibalsOnRightBank > missionariesOnRightBank + action.count('M') and missionariesOnRightBank!= 0 and action in possible_actions:
+                    possible_actions.remove(action)
+                """Checking if the number of missionaries remaining on the left bank will be outnumbered by the cannibals on the left bank, but its okay if missionaries == 0 after action"""
+                if missionariesOnLeftBank - action.count('M') < cannibalsOnLeftBank - action.count('C') and missionariesOnLeftBank - action.count('M') != 0 and action in possible_actions:
+                    possible_actions.remove(action)
+        else:
+            for action in possible_actions[:]:
+                """Checking to see if the cannibals to be sent back to the left bank will out number the missionaries on the left bank"""
+                if (action.count('C') + cannibalsOnLeftBank) > (missionariesOnLeftBank + action.count('M')) and missionariesOnLeftBank != 0 and action in possible_actions:
+                    possible_actions.remove(action)
+                """Checking to see if the number missionaries remaining on the right bank will be outnumbered by the cannibals on the right bank"""
+                if (missionariesOnRightBank - action.count('M') < cannibalsOnRightBank - action.count('C')) and missionariesOnRightBank - action.count('M') != 0 and action in possible_actions:
                     possible_actions.remove(action)
                 
         return possible_actions
 
 
     def result(self, state, action):
-        return True
+        if state[2] == True:
+            numOfMissionary = state[0] - action.count('M')
+            numOfCannibal = state[1] - action.count('C')
+        else:
+            numOfMissionary = state[0] + action.count('M')
+            numOfCannibal = state[1] + action.count('C')
+
+        boatLocation = not state[2]
+
+        newState = (numOfMissionary, numOfCannibal, boatLocation)
+
+        return newState
 
 
 if __name__ == '__main__':
-    #mc = MissCannibalsVariant(4,4)
-    # print(mc.actions((3, 3, True))) # Test your code as you develop! This should return  ['MC', 'MMM']
-
-    mc = MissCannibalsVariant()
-    print(mc.actions((3, 3, True)))
-    
-    
+    mc = MissCannibalsVariant(4,4)
+    #print(mc.actions((3, 3, True))) # Test your code as you develop! This should return  ['MC', 'MMM']
 
     # path = depth_first_graph_search(mc).solution()
     # print(path)
-    # path = breadth_first_graph_search(mc).solution()
-    # print(path)
+    path = breadth_first_graph_search(mc).solution()
+    print(path)
 
     
